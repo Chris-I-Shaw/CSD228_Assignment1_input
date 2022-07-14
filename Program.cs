2de11b9499e91845623ba92bd66396a8300ec12c
Chris Shaw <103132499+Chris-I-Shaw@users.noreply.github.com>
2022-07-14 13:15:56 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Homework 
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            string? x = null;
+            
+            Console.WriteLine(IsNull(x));
+        } 
+        public static string IsNull(string? x){
+            string? result = x;
+            return result == null ? result?.GetValue() ?? -1 : 0;
+        }
+    }
+}
